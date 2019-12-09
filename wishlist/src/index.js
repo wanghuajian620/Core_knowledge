49bf3877a040801dc1c947c5528add2e6b7537b5
wanghuajian620 <154659041@qq.com>
2019-12-09 10:30:05 +0800

    未进行组件拆分的 愿望列表，可增加，可删除

--- wishlist/src/index.js
+++ wishlist/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+// import App from './App';
+// import './index.css';
+import WishList from './wishlist/index';
 
 ReactDOM.render(
-  <App />,
+  <WishList />,
   document.getElementById('root')
 );
---
+++ wishlist/src/wishlist/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+class WishList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      list: [],
+    }
+  }
+  writeWish = (e) => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  addWish = () => {
+    this.setState({
+      list: [...this.state.list, this.state.value],
+      value: ''
+    })
+  }
+
+  deleteItem = (index) => {
+    let list = this.state.list;
+    list.splice(index,1)
+    this.setState({
+      list: list
+    })
+  }
+  render() { 
+    return ( <React.Fragment>
+      <input type='text' value={this.state.value} onChange={this.writeWish}></input>
+      <button type='button' style={{ backgroundColor: 'red', color: 'white' }} onClick={this.addWish}>新增2020wish清单</button>
+      <ul>
+        {this.state.list.map((item, index) => {
+          return <li key={item + index} onClick={() => {this.deleteItem(index)}}>{item}</li>
+        })}
+      </ul>
+    </React.Fragment> );
+  }
+}
+ 
+export default WishList;

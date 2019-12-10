a0d8ea0e03d58efb80d51877b14b47a92fd4d588
wanghuajian620 <154659041@qq.com>
2019-12-10 16:04:46 +0800

    加入 静态类型检测，以及属性默认值defaultProps

--- wishlist/src/wishlist/ListContent.js
+++ wishlist/src/wishlist/ListContent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class ListContent extends Component {
   constructor(props) {
@@ -12,9 +13,19 @@ class ListContent extends Component {
   }
   render() { 
     return ( 
-      <li onClick={this.handleClick}>{this.props.content}</li>
+    <li onClick={this.handleClick}>Hello{this.props.name}，你的本月清单：{this.props.content}</li>
     );
   }
 }
- 
+
+ListContent.defaultProps = {
+  name: '华健'
+}
+
+ListContent.propTypes = {
+  content: PropTypes.string,
+  index: PropTypes.number,
+  deleteItem: PropTypes.func,
+}
+
 export default ListContent;

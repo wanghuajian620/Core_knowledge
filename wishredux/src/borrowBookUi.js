45105710122f9ddf641fcc0ef1eacf9df2becc21
wanghuajian620 <154659041@qq.com>
2019-12-18 11:50:51 +0800

    将有状态组件改为无状态组件，适用于纯展示UI的组件
    无状态组件没生命周期，所以性能上也会好点

--- wishredux/src/borrowBookUi.js
+++ wishredux/src/borrowBookUi.js
@@ -3,39 +3,36 @@
  *        Initial:  2019/12/17         Author:  Wang Huajian
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
 
-class BorrowBookUi extends Component {
-  
-  render() { 
-    return ( 
-      <div style={{ margin: '10px' }}>
-        <div>
-          冷知识：《死生契阔，与子成说。执子之手，与子偕老》是形容战友情的。
-        </div>
-        <div>
-          <Input
-            placeholder={this.props.inputValue}
-            value={this.props.inputValue}
-            style={{ width: '250px', marginRight: '10px' }}
-            onChange={this.props.changeInputValue}
-          />
-          <Button type='primary' style={{ backgroundColor: 'red'}} onClick={this.props.clickBtn}>添加</Button>
-        </div>
-        <div>
-          <List
-            size="large"
-            bordered
-            dataSource={this.props.list}
-            renderItem={(item, index) => (<List.Item onClick={() => {this.props.deleteItem(index)}}>{item}</List.Item>)}
-            style={{ width: '350px', marginTop: '20px'}}
-          />
-        </div>
-      </div> 
-    );
-  }
+const BorrowBookUi = (props) => {
+  return ( 
+    <div style={{ margin: '10px' }}>
+      <div>
+        冷知识：《死生契阔，与子成说。执子之手，与子偕老》是形容战友情的。
+      </div>
+      <div>
+        <Input
+          placeholder={props.inputValue}
+          value={props.inputValue}
+          style={{ width: '250px', marginRight: '10px' }}
+          onChange={props.changeInputValue}
+        />
+        <Button type='primary' style={{ backgroundColor: 'red'}} onClick={props.clickBtn}>添加</Button>
+      </div>
+      <div>
+        <List
+          size="large"
+          bordered
+          dataSource={props.list}
+          renderItem={(item, index) => (<List.Item onClick={() => {props.deleteItem(index)}}>{item}</List.Item>)}
+          style={{ width: '350px', marginTop: '20px'}}
+        />
+      </div>
+    </div> 
+  );
 }
  
 export default BorrowBookUi;

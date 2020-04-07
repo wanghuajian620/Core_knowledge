4f457e3ecbda107267f28301179fbca5bad021ff
wanghuajian620 <154659041@qq.com>
2020-04-07 16:18:28 +0800

    新增 高阶组件 属性代理、反向继承案例

--- hoccomponent/src/index.js
+++ hoccomponent/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './main';
+// 新旧版本的ref使用
+// import Main from './ref';
+// 高阶组件场景一、属性代理的三个案例
+// import NormalComponent from './hoc/wrapperComponent';
+// 高阶组件场景二、反向继承之渲染劫持，改变组件树的样式
+import NormalComponent from './hoc/renderHijack';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Main />
+    <NormalComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
-

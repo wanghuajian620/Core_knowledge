ae5d03e1a04e76e3a162c4f1b3a0ab087c95a783
wanghuajian620 <154659041@qq.com>
2020-09-21 14:05:27 +0800

    nothing

--- hoccomponent/src/hoc/wrapperComponent.js
+++ hoccomponent/src/hoc/wrapperComponent.js
@@ -6,80 +6,80 @@ import React, {Component} from 'react';
 
 // 属性代理又分三个场景案例：
 // 案例一 获取被包裹组件的refs,可直接更改被包裹组件的显示内容
-// const HOC = (WrapperComponent) => {
-//   return class extends Component {
-//     wrapper = React.createRef();
-//     componentDidMount() {
-//       console.log(this.wrapper.current.state);
-//       this.wrapper.current.setState({ text: '通过高阶组件改变的内容' });
-//     }
-//     render() {
-//       return (
-//         <WrapperComponent {...this.props} ref={this.wrapper} />
-//       );
-//     }
-//   }
-// }
+const HOC = (WrapperComponent) => {
+  return class extends Component {
+    wrapper = React.createRef();
+    componentDidMount() {
+      console.log(this.wrapper.current.state);
+      this.wrapper.current.setState({ text: '通过高阶组件改变的内容' });
+    }
+    render() {
+      return (
+        <WrapperComponent {...this.props} ref={this.wrapper} />
+      );
+    }
+  }
+}
 
 // 使用高阶组件时有两种写法
 // 写法一如下
-// class NormalComponent extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       text: 'Hello Shang Hai'
-//     }
-//   }
-//   render() {
-//     return (
-//       <div>{this.state.text}</div>
-//     );
-//   }
-// }
+class NormalComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Hello Shang Hai'
+    }
+  }
+  render() {
+    return (
+      <div>{this.state.text}</div>
+    );
+  }
+}
 
-// export default HOC(NormalComponent);
+export default HOC(NormalComponent);
 
 // 写法二如下(暂不支持修饰器写法，需要安装插件)
-// @HOC
-// class NormalComponent extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       text: 'Hello Shang Hai'
-//     }
-//   }
-//   render() {
-//     return (
-//       <div>{this.state.text}</div>
-//     );
-//   }
-// }
-// export default NormalComponent;
+@HOC
+class NormalComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Hello Shang Hai'
+    }
+  }
+  render() {
+    return (
+      <div>{this.state.text}</div>
+    );
+  }
+}
+export default NormalComponent;
  
 
 
 
 // 案例二 抽象state，把非受控组件转变为受控组件
-// const HOC = (WrapperComponent) => {
-//   return class extends Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {
-//         value: ''
-//       }
-//     }
-//     onChange = (e) => {
-//       this.setState({
-//         value: e.target.value
-//       })
-//     }
-//     render() {
-//       return (
-//         <WrapperComponent {...this.props} value={this.state.value} onChange={this.onChange} />
-//       );
-//     }
-//   }
-// }
+const HOC = (WrapperComponent) => {
+  return class extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        value: ''
+      }
+    }
+    onChange = (e) => {
+      this.setState({
+        value: e.target.value
+      })
+    }
+    render() {
+      return (
+        <WrapperComponent {...this.props} value={this.state.value} onChange={this.onChange} />
+      );
+    }
+  }
+}
 
 // 函数式组件不能用装饰器，装饰器只能修饰类
 // const NormalComponent = (props) => <input {...props} type="text"/>
--- hoccomponent/src/index.js
+++ hoccomponent/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // 新旧版本的ref使用
-import Main from './ref';
+// import Main from './ref';
 // 高阶组件场景一、属性代理的三个案例
-import NormalComponent from './hoc/wrapperComponent';
+// import NormalComponent from './hoc/wrapperComponent';
 // 高阶组件场景二、反向继承之渲染劫持，改变组件树的样式
-// import NormalComponent from './hoc/renderHijack';
+import NormalComponent from './hoc/renderHijack';
 // import AA from './isntance';
 
 ReactDOM.render(
   <React.Fragment>
     <NormalComponent />
     {/* <AA /> */}
-    <Main />
+    {/* <Main /> */}
   </React.Fragment>,
   document.getElementById('root')
 );
